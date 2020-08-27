38d96ca409b09b74ebfd80c16d2a125cade2897a
Alper <37253853+zozdag@users.noreply.github.com>
2020-08-27 18:33:39 +0300

    Added Delete Function with Context Api
    You can delete a book from state easly

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { Component, useState, useContext } from 'react'
+import { BookContext } from './context'
 
 
 
 const Books = ({id,bookName,author,price})  => {
+    const [books, setBooks] = useContext(BookContext);
 
+    const DeleteBook = (id) =>{
+        console.log(id);
+        const newBooks = books.filter((item) => item.id !== id);
+        setBooks(newBooks);
+    }
+   
 
         return (
-            
+          
                             <tr>
 
                                 <th scope="row">{id}</th>
                                 <td> {bookName} </td>
                                 <td> {author} </td>
                                 <td> {price}₺</td>
+                                <td><button className="btn btn-danger" onClick={() => DeleteBook(id)} >Delete</button></td>
                                
                             </tr>
         )
